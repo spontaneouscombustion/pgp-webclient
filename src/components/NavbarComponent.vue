2d01ee53c069eac7595819b574cc198ebb5a14a4
non-human <113906507+spontaneouscombustion@users.noreply.github.com>
2024-03-01 11:01:02 -0500

    style(navlink): add home menu icon

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -23,7 +23,12 @@
         </div>
         <div class="navbar-end">
             <ul class="menu menu-horizontal px-1">
-                <li><RouterLink :to="{ name: 'home' }">Home</RouterLink></li>
+                <li>
+                    <RouterLink :to="{ name: 'home' }">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current size-5" viewBox="0 0 16 16"><path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/></svg>
+                        Home
+                    </RouterLink>
+                </li>
             </ul>
         </div>
     </div>
