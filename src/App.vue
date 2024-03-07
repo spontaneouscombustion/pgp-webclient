af16636a4deb6614122bf507f87e16772b9d15c8
non-human <113906507+spontaneouscombustion@users.noreply.github.com>
2024-03-07 01:37:37 -0500

    feat: show toast on onOfflineReady and onNeedRefresh event

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,68 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { type PrivateKey } from 'openpgp'
+import { provide, ref, onMounted } from 'vue'
+import { privateKeySymbol } from '@/keys'
+import { registerSW } from 'virtual:pwa-register'
 import NavbarComponent from '@/components/NavbarComponent.vue'
 import PrivateKeyComponent from '@/components/PrivateKeyComponent.vue'
-import { provide, ref } from 'vue'
-import { privateKeySymbol } from '@/keys'
+import ToastComponent from '@/components/ToastComponent.vue'
+import AlertComponent from '@/components/AlertComponent.vue'
+
+const toastAlerts = ref<
+  {
+    type: 'alert-success' | 'alert-info' | 'alert-warning' | 'alert-error'
+    message: string
+    buttons?: {
+      name: string
+      classes: string
+      action: (params: any, event: Event) => any | void
+    }[]
+  }[]
+>([])
+
+onMounted(() => {
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      toastAlerts.value.push({
+        message: 'A new update has been detected!',
+        type: 'alert-success',
+        buttons: [
+          {
+            name: 'Later',
+            classes: 'btn-success',
+            action: (index: number) => {
+              toastAlerts.value.splice(index, 1)
+            }
+          },
+          {
+            name: 'Reload',
+            classes: 'btn-primary',
+            action: async (index: number) => {
+              updateSW()
+              toastAlerts.value.splice(index, 1)
+            }
+          }
+        ]
+      })
+    },
+    onOfflineReady() {
+      toastAlerts.value.push({
+        message: 'This app can now be used even without internet connection!',
+        type: 'alert-info',
+        buttons: [
+          {
+            name: 'OK',
+            classes: 'btn-info',
+            action: (index: number) => {
+              toastAlerts.value.splice(index, 1)
+            }
+          }
+        ]
+      })
+    }
+  })
+})
 
 const privateKeyObject = ref<PrivateKey>()
 
@@ -38,4 +96,23 @@ provide(privateKeySymbol, {
       </div>
     </div>
   </div>
+  <ToastComponent>
+    <AlertComponent
+      v-for="(alert, index) in toastAlerts"
+      :key="index"
+      :type="alert.type"
+      :message="alert.message"
+    >
+      <div>
+        <button
+          v-for="(button, i) in alert.buttons"
+          :key="i"
+          class="btn btn-sm"
+          :class="button.classes"
+          v-text="button.name"
+          @click="(e) => button.action(index, e)"
+        ></button>
+      </div>
+    </AlertComponent>
+  </ToastComponent>
 </template>
